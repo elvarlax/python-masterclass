d1d76bf1a8e1f35f9e10ec34d647382d1b4faf3a
Elvar Laxdal <elvarlax@gmail.com>
2020-03-29 13:27:31 +0000

    Star Args Challenge

---
+++ Section 11/Star Args Challenge/star_args_challenge.py
@@ -0,0 +1,28 @@
+"""
+Section 11 Challenge - Star Args
+Write a function called build_tuple that takes a variable number of arguments,
+and returns a tuple containing the values passed to it.
+
+Example of input to the function, and the expected output:
+
+message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")
+print(type(message_tuple))
+print(message_tuple)
+
+number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+print(type(number_tuple))
+print(number_tuple)
+"""
+
+
+def build_tuple(*args):
+    return args
+
+
+message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")
+print(type(message_tuple))
+print(message_tuple)
+
+number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+print(type(number_tuple))
+print(number_tuple)
