309c7b72d2f7dafd72448809213ef9807511b5b0
Elvar Laxdal <elvarlax@gmail.com>
2020-04-02 15:56:05 +0000

    Nested Comprehensions Challenge

---
+++ Section 12/Nested Comprehensions Challenge/nested_comprehensions_challenge.py
@@ -0,0 +1,18 @@
+"""
+Section 12 Challenge - Nested Comprehensions 1
+In an early video, we used a for loop to print the times tables, for values from 1 to 10.
+That was nested loop, which appears below.
+
+The challenge is to use a nested comprehension, to produce the same values.
+You can iterate over the list, to produce the same output as the for loop, or just print out the list.
+"""
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i, i * j)
+
+print("*" * 10)
+
+times = [(i, i * j) for i in range(1, 11) for j in range(1, 11)]
+
+for x, y in times:
+    print(x, y)
