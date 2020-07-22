1d35897ca9b042ac945425bf24328e152c8b2a17
Elvar Laxdal <elvarlax@gmail.com>
2020-07-22 00:17:25 +0000

    Coding Exercise 13

---
+++ Section 05/Adding items to lists/coding_exercise13.py
@@ -0,0 +1,33 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac- Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+for plant in data:
+    if "Flower" in plant:
+        flowers.append(plant)
+    elif "Shrub" in plant:
+        shrubs.append(plant)
+
+print(flowers)
+print(shrubs)
