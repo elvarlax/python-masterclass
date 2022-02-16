1c56299dbbaa49e9bac2a8d88958027ae4d91c9a
Elvar Laxdal <elvarlax@gmail.com>
2022-02-16 19:48:06 +0100

    Coding Exercise 16

--- Section 06/16 - Sum even or odd numbers in a range/coding_exercise16.py
+++ Section 06/16 - Sum even or odd numbers in a range/coding_exercise16.py
@@ -14,3 +14,28 @@ Examples:
     2. sum_eo(7, 'o') should return 9, since 1 + 3 + 5 = 9
     3. sum_eo(11, 'spam') should return -1.
 """
+
+
+def sum_eo(n, t):
+    """
+    Sum even or odd numbers in range.
+    Return the sum of even or odd natural numbers, in the range 1..n-1.
+
+    :param n: The endpoint of the range. The numbers from 1 to n-1 will be summed.
+    :param t: 'e' to sum even numbers, 'o' to sum odd numbers.
+    :return: The sum of the even or odd numbers in the range.
+             Returns -1 if `t` is not 'e' or 'o'.
+    """
+    if t == "e":
+        start = 2
+    elif t == 'o':
+        start = 1
+    else:
+        return -1
+
+    return sum(range(start, n, 2))
+
+
+print(sum_eo(10, 'e'))
+print(sum_eo(7, 'o'))
+print(sum_eo(11, 'spam'))
