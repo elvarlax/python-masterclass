5c1db5458c64e13a3f9f54e3b70889db215a17b1
Elvar Laxdal <elvarlax@gmail.com>
2022-02-19 20:29:23 +0100

    Coding Exercise 19

--- Section 06/19 - Variable number of arguments/coding_exercise19.py
+++ Section 06/19 - Variable number of arguments/coding_exercise19.py
@@ -17,3 +17,19 @@ Values              Result
 12.5, 3.147, 98.1   113.747
 1.1, 2.2, 5.5       8.8
 """
+
+
+def sum_numbers(*values: float) -> float:
+    """
+    Calculates the sum of all the numbers passed as arguments.
+
+    :param values: The list of numbers as arguments.
+    :return: The sum of all the passed arguments.
+    """
+    return sum(values)
+
+
+print(sum_numbers(1, 2, 3))
+print(sum_numbers(8, 20, 2))
+print(sum_numbers(12.5, 3.147, 98.1))
+print(sum_numbers(1.1, 2.2, 5.5))
