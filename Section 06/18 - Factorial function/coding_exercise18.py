a92135025e29a9d5bd6a4e20b5a85b46d6ec2b03
Elvar Laxdal <elvarlax@gmail.com>
2022-02-19 20:28:18 +0100

    Coding Exercise 18

--- Section 06/18 - Factorial function/coding_exercise18.py
+++ Section 06/18 - Factorial function/coding_exercise18.py
@@ -50,3 +50,21 @@ Your results should be:
 34 295232799039604140847618609643520000000
 35 10333147966386144929666651337523200000000
 """
+
+
+def factorial(n: int) -> int:
+    """
+    Return n! (0! is 1).
+
+    Valid for `n` in the range 0 to 998 (inclusive).
+    Larger values of `n` will cause a RecursionError.
+    """
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+for i in range(36):
+    print(i, factorial(i))
+
