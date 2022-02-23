e362533b97009e6c22d5a680a32c9afacdd8a8de
Elvar Laxdal <elaxdal@bettercollective.com>
2022-02-23 14:48:46 +0100

    Coding Exercise 20

--- Section 07/20 - Character counter/coding_exercise20.py
+++ Section 07/20 - Character counter/coding_exercise20.py
@@ -26,7 +26,12 @@ word_count = {}
 # Text string
 text = "Later in the course, you'll see how to use the collections Counter class."
 
-# Your code goes here ...
+for char in text.casefold():
+    if char.isalnum():
+        if char not in word_count:
+            word_count[char] = 1
+        else:
+            word_count[char] += 1
 
 # Printing the dictionary
 for letter, count in sorted(word_count.items()):
